aa000e861cf6ece1e4ced8550eec0f37e1012358
Pankova <pankkoo89@gmail.com>
2024-05-30 02:13:44 +0300

    feat: add main part of app

---
+++ Counter/EvenNumbersCounter.cs
@@ -0,0 +1,18 @@
+﻿namespace Counter
+{
+    public class EvenNumbersCounter
+    {
+        public static int CountEvenNumbers(int[] numbers)
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ test-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Counter;
+namespace test_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 2, 3, 4 };
+            Console.WriteLine("Массив:");
+            numbers.ToList().ForEach(num => Console.Write(num+"\t"));
+            Console.WriteLine("\nКоличество чётных чисел - "+EvenNumbersCounter.CountEvenNumbers(numbers));
+        }
+    }
+}
